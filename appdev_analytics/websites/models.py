138c79bb4a6a60be05f68a512b58d421d2669cb4
ethanandrews <eandrews@whoi.edu>
2021-05-11 11:16:04 -0400

    add args to make ga4 report method reusable, accept multiple filters

--- appdev_analytics/websites/models.py
+++ appdev_analytics/websites/models.py
@@ -19,22 +19,45 @@ class Website(models.Model):
     def __str__(self):
         return self.name
 
-    def get_ga4_data(self):
-        """Runs a simple report on a Google Analytics 4 property."""
+    def get_ga4_data(
+        self,
+        dimensions=["country"],
+        metrics=["activeUsers"],
+        start_date="2020-03-31",
+        end_date="today",
+    ):
+        """Runs a report on a Google Analytics 4 property."""
         # Using a default constructor instructs the client to use the credentials
         # specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
         client = BetaAnalyticsDataClient()
 
+        # ex. dimensions: date, city
+        dimensions_formatted = []
+        for dim in dimensions:
+            new_dim = Dimension(name=dim)
+            dimensions_formatted.append(new_dim)
+
+        # ex. metrics: screenPageViews, activeUsers
+        metrics_formatted = []
+        for met in metrics:
+            new_met = Metric(name=met)
+            metrics_formatted.append(new_met)
+
         request = RunReportRequest(
             property=f"properties/{self.ga4_property_id}",
-            dimensions=[Dimension(name="city")],
-            metrics=[Metric(name="activeUsers")],
-            date_ranges=[DateRange(start_date="2020-03-31", end_date="today")],
+            dimensions=dimensions_formatted,
+            metrics=metrics_formatted,
+            date_ranges=[DateRange(start_date=start_date, end_date=end_date)],
         )
         response = client.run_report(request)
 
         print("Report result:")
         for row in response.rows:
-            print(row.dimension_values[0].value, row.metric_values[0].value)
+            for dimension_value in row.dimension_values:
+                print(dimension_value.value)
 
-        return self
+            for metric_value in row.metric_values:
+                print(metric_value.value)
+            # print(row.dimension_values[0].value, row.metric_values[0].value)
+
+        return response
