4cf6cfaa4a138e293300c569716a5c71eece5726
ethanandrews <eandrews@whoi.edu>
2021-05-23 14:11:14 -0400

    check for empty dataframe

--- appdev_analytics/websites/models.py
+++ appdev_analytics/websites/models.py
@@ -150,36 +150,39 @@ class Website(models.Model):
                 print(e)
                 continue
 
-        logs_df["bytessent"] = logs_df["bytessent"].astype(int)
-        new_df = logs_df.groupby(["url", pd.Grouper(key="dateandtime", freq="D")]).agg(
-            {
-                "bytessent": "sum",
-                "dateandtime": "count",
-            }
-        )
-        new_df.columns = ["bytessent", "hits"]
-        new_df = new_df.reset_index()
+        if not logs_df.empty():
+            logs_df["bytessent"] = logs_df["bytessent"].astype(int)
+            new_df = logs_df.groupby(
+                ["url", pd.Grouper(key="dateandtime", freq="D")]
+            ).agg(
+                {
+                    "bytessent": "sum",
+                    "dateandtime": "count",
+                }
+            )
+            new_df.columns = ["bytessent", "hits"]
+            new_df = new_df.reset_index()
 
-        for index, row in new_df.iterrows():
-            print(row)
-            datapoint = DataPoint.objects.filter(
-                date_logged=row["dateandtime"].date(), url=row["url"]
-            ).last()
+            for index, row in new_df.iterrows():
+                print(row)
+                datapoint = DataPoint.objects.filter(
+                    date_logged=row["dateandtime"].date(), url=row["url"]
+                ).last()
 
-            if datapoint:
-                # date/url combination already has a record, need to update bytes_sent/hits
-                datapoint.bytes_sent = datapoint.bytes_sent + row["bytessent"]
-                datapoint.hits = datapoint.hits + row["hits"]
-                datapoint.save()
-            else:
-                # else create new Datapoint
-                datapoint = DataPoint.objects.create(
-                    website=self,
-                    date_logged=row["dateandtime"].date(),
-                    url=row["url"],
-                    bytes_sent=row["bytessent"],
-                    hits=row["hits"],
-                )
+                if datapoint:
+                    # date/url combination already has a record, need to update bytes_sent/hits
+                    datapoint.bytes_sent = datapoint.bytes_sent + row["bytessent"]
+                    datapoint.hits = datapoint.hits + row["hits"]
+                    datapoint.save()
+                else:
+                    # else create new Datapoint
+                    datapoint = DataPoint.objects.create(
+                        website=self,
+                        date_logged=row["dateandtime"].date(),
+                        url=row["url"],
+                        bytes_sent=row["bytessent"],
+                        hits=row["hits"],
+                    )
         return "Data points updated"
 
 
