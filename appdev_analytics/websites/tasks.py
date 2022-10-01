96b11333e3f82728ccba5a5e646cfc3f37da5e4f
ethanandrews <eandrews@whoi.edu>
2022-10-01 16:00:00 -0400

    break each site data download from s3 into separate celery tasks

--- appdev_analytics/websites/tasks.py
+++ appdev_analytics/websites/tasks.py
@@ -3,9 +3,25 @@ from celery import shared_task
 from .models import Website
 
 
+@shared_task(time_limit=1800, soft_time_limit=1800, bind=True)
+def download_data_update(self, site_id=None):
+    print(site_id)
+    if not site_id:
+        return False
+
+    try:
+        site = Website.objects.get(id=site_id)
+    except Website.DoesNotExist:
+        return False
+
+    site.update_download_data()
+    print(f"{site} logs parsed")
+    return True
+
+
 @shared_task(time_limit=7200, soft_time_limit=7200)
 def run_download_data_updates():
     sites = Website.objects.filter(is_active=True)
     for site in sites:
-        site.update_download_data()
-        print(f"{site} logs parsed")
+        print(f"{site} queued for processing - {site.id}")
+        download_data_update.delay(site.id)
